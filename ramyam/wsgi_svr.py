dd24d28c0c0e2bf06d705a12edce215b0330e042
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2015-02-26 12:34:31 -0800

    make more testable

--- ramyam/wsgi_svr.py
+++ ramyam/wsgi_svr.py
@@ -6,6 +6,8 @@ from gevent.pywsgi import WSGIServer
 
 from .wsgi_mid import wrap, RamlMiddleware
 
+Offset = int(os.environ.get('PORT_OFFSET','8000'))
+
 def spew():
     print("*"*40)
     tb.print_exc()
@@ -25,10 +27,10 @@ def serve_http(app,port):
     except: raise spew()
 
 def serve_both(app,dd):
-    serve_https(app,8443,dd).start()
-    return serve_http(app,8080)
+    serve_https(app,Offset+443,dd).start()
+    return serve_http(app,Offset+80)
 
-if __name__ == '__main__':
+def main(raml_obj=None):
     def test_application(env, start_response):
         if env['PATH_INFO'] == '/':
             start_response('200 OK', [('Content-Type', 'text/html')])
@@ -43,5 +45,8 @@ if __name__ == '__main__':
             start('200 OK', [('Content-Type', 'text/html')])
             return ["<b>hello worldxxxx</b>"]
         pass
-    serve_both( RamlMiddleware(test_application,TestRamlObject()),
+    raml_obj = raml_obj or TestRamlObject
+    serve_both( RamlMiddleware(test_application,raml_obj()),
                 os.environ.get('DD','data/') ).serve_forever()
+
+if __name__ == '__main__':main()
--- run.sh
+++ run.sh
@@ -6,46 +6,71 @@
 #echo Trying python...
 #./bin/raml-client.js -l python     -o igpy -e data/instagram-api.raml
 
-if [ -d .v ]; then
-  echo exists
+if [ "$3" == "-noinstall" ]; then
+  echo no install
 else
-  echo not exist.  adding.
-  virtualenv .v
+  if [ -d .v ]; then
+    echo exists
+  else
+    echo not exist.  adding.
+    virtualenv .v
+  fi
+
+  source .v/bin/activate
+  pip install -r requirements.txt
 fi
 
-source .v/bin/activate
-pip install -r requirements.txt
+all=
+if [ "$1" == "" ]; then
+  all=yes
+fi
+if [ "$1" == "all" ]; then
+  all=yes
+fi
 
-echo 'Trying python (bitly-api.raml)...'
-python -mramyam.gen_client -y data/bitly-api.raml >bitly_api.py
-python -mramyam.gen_server -y data/bitly-api.raml >bitly_svr.py
+if [ "$2" == "ig" ]; then
+  echo 'Trying python (instagram-api.raml)...'
+  python -mramyam.gen_client -y data/instagram-api.raml >instagram_api.py
+  python -mramyam.gen_server -y data/instagram-api.raml >instagram_svr.py
+fi
 
-echo 'Trying python (box-api.raml)...'
-python -mramyam.gen_client -y data/box-api.raml >box_api.py
-python -mramyam.gen_server -y data/box-api.raml >box_svr.py
+if [ "$all" == "yes" ]; then
+  echo 'Trying python (bitly-api.raml)...'
+  python -mramyam.gen_client -y data/bitly-api.raml >bitly_api.py
+  python -mramyam.gen_server -y data/bitly-api.raml >bitly_svr.py
 
-echo 'Trying python (instagram-api.raml)...'
-python -mramyam.gen_client -y data/instagram-api.raml >instagram_api.py
-python -mramyam.gen_server -y data/instagram-api.raml >instagram_svr.py
+  echo 'Trying python (box-api.raml)...'
+  python -mramyam.gen_client -y data/box-api.raml >box_api.py
+  python -mramyam.gen_server -y data/box-api.raml >box_svr.py
 
-echo 'Trying python (twilio-rest-api.raml)...'
-python -mramyam.gen_client -y data/twilio-rest-api.raml >twilio_api.py
-python -mramyam.gen_server -y data/twilio-rest-api.raml >twilio_svr.py
+  echo 'Trying python (instagram-api.raml)...'
+  python -mramyam.gen_client -y data/instagram-api.raml >instagram_api.py
+  python -mramyam.gen_server -y data/instagram-api.raml >instagram_svr.py
 
-echo 'Trying python (github-api-v3.raml)...'
-python -mramyam.gen_client -y data/github-api-v3.raml >github_api.py
-python -mramyam.gen_server -y data/github-api-v3.raml >gibhub_svr.py
+  echo 'Trying python (twilio-rest-api.raml)...'
+  python -mramyam.gen_client -y data/twilio-rest-api.raml >twilio_api.py
+  python -mramyam.gen_server -y data/twilio-rest-api.raml >twilio_svr.py
 
-echo 'Trying python (stripe-api.raml)...'
-python -mramyam.gen_client -y data/stripe-api.raml >stripe_api.py
-python -mramyam.gen_server -y data/stripe-api.raml >stripe_svr.py
+  echo 'Trying python (github-api-v3.raml)...'
+  python -mramyam.gen_client -y data/github-api-v3.raml >github_api.py
+  python -mramyam.gen_server -y data/github-api-v3.raml >gibhub_svr.py
 
-echo 'Trying python (twitter-rest-api.raml)...'
-python -mramyam.gen_client -y data/twitter-rest-api.raml >twitter_rest_api.py
-python -mramyam.gen_server -y data/twitter-rest-api.raml >twitter_rest_svr.py
+  echo 'Trying python (stripe-api.raml)...'
+  python -mramyam.gen_client -y data/stripe-api.raml >stripe_api.py
+  python -mramyam.gen_server -y data/stripe-api.raml >stripe_svr.py
 
-echo 'Trying python (twitter-api.raml)...'
-python -mramyam.gen_client -y data/twitter.raml >twitter_api.py
-python -mramyam.gen_server -y data/twitter.raml >twitter_svr.py
+  echo 'Trying python (twitter-rest-api.raml)...'
+  python -mramyam.gen_client -y data/twitter-rest-api.raml >twitter_rest_api.py
+  python -mramyam.gen_server -y data/twitter-rest-api.raml >twitter_rest_svr.py
 
-echo Complete.
+  echo 'Trying python (twitter-api.raml)...'
+  python -mramyam.gen_client -y data/twitter.raml >twitter_api.py
+  python -mramyam.gen_server -y data/twitter.raml >twitter_svr.py
+
+  echo Complete.
+fi
+
+if [ "$1" == "run" ]; then
+  echo 'Running server...'
+  PORT_OFFSET=9000 python -mramyam.wsgi_svr
+fi
