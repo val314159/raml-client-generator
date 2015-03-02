a3b6d95fd7bcb5a9fad3643e593653be7eb1dc6f
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2015-03-02 13:18:50 -0800

    better tests

--- run.sh
+++ run.sh
@@ -22,6 +22,9 @@ fi
 
 source .v/bin/activate
 
+export INDIR=data
+mkdir -p gen/languages/js
+
 all=
 if [ "$1" == "" ]; then
   all=yes
@@ -32,51 +35,51 @@ fi
 
 if [ "$2" == "ig" ]; then
   echo 'Trying python (instagram-api.raml)...'
-  python -mramyam.gen_client -y data/instagram-api.raml -intermediate >instagram_api.json
-  python -mramyam.gen_client -y data/instagram-api.raml >instagram_api.py
-  python -mramyam.gen_server -y data/instagram-api.raml >instagram_svr.py
+  python -mramyam.gs -y instagram-api.raml -intermediate >gen/languages/js/instagram-api.json
+  python -mramyam.gs -y instagram-api.raml
+  #python -mramyam.gen_server -y data/instagram-api.raml >instagram_svr.py
 fi
 
 if [ "$all" == "yes" ]; then
   echo 'Trying python (bitly-api.raml)...'
-  python -mramyam.gen_client -y data/bitly-api.raml -intermediate >bitly_api.json
-  python -mramyam.gen_client -y data/bitly-api.raml >bitly_api.py
-  python -mramyam.gen_server -y data/bitly-api.raml >bitly_svr.py
+  python -mramyam.gs -y bitly-api.raml -intermediate >gen/languages/js/bitly-api.json
+  python -mramyam.gs -y bitly-api.raml
+  #python -mramyam.gen_server -y data/bitly-api.raml >bitly_svr.py
 
   echo 'Trying python (box-api.raml)...'
-  python -mramyam.gen_client -y data/box-api.raml -intermediate >box_api.json
-  python -mramyam.gen_client -y data/box-api.raml >box_api.py
-  python -mramyam.gen_server -y data/box-api.raml >box_svr.py
+  python -mramyam.gs -y box-api.raml -intermediate >gen/languages/js/box-api.json
+  python -mramyam.gs -y box-api.raml >box_api.py
+  #python -mramyam.gen_server -y data/box-api.raml >box_svr.py
 
   echo 'Trying python (instagram-api.raml)...'
-  python -mramyam.gen_client -y data/instagram-api.raml -intermediate >instagram_api.json
-  python -mramyam.gen_client -y data/instagram-api.raml >instagram_api.py
-  python -mramyam.gen_server -y data/instagram-api.raml >instagram_svr.py
+  python -mramyam.gs -y instagram-api.raml -intermediate >gen/languages/js/instagram-api.json
+  python -mramyam.gs -y instagram-api.raml
+  #python -mramyam.gen_server -y data/instagram-api.raml >instagram_svr.py
 
   echo 'Trying python (twilio-rest-api.raml)...'
-  python -mramyam.gen_client -y data/twilio-rest-api.raml -intermediate >twilio_api.json
-  python -mramyam.gen_client -y data/twilio-rest-api.raml >twilio_api.py
-  python -mramyam.gen_server -y data/twilio-rest-api.raml >twilio_svr.py
+  python -mramyam.gs -y twilio-rest-api.raml -intermediate >gen/languages/js/twilio.json
+  python -mramyam.gs -y twilio-rest-api.raml >twilio_api.py
+  #python -mramyam.gen_server -y data/twilio-rest-api.raml >twilio_svr.py
 
   echo 'Trying python (github-api-v3.raml)...'
-  python -mramyam.gen_client -y data/github-api-v3.raml -intermediate >github_api.json
-  python -mramyam.gen_client -y data/github-api-v3.raml >github_api.py
-  python -mramyam.gen_server -y data/github-api-v3.raml >gibhub_svr.py
+  python -mramyam.gs -y github-api-v3.raml -intermediate >gen/languages/js/github-v3.json
+  python -mramyam.gs -y github-api-v3.raml >github_api.py
+  #python -mramyam.gen_server -y data/github-api-v3.raml >gibhub_svr.py
 
   echo 'Trying python (stripe-api.raml)...'
-  python -mramyam.gen_client -y data/stripe-api.raml -intermediate >stripe_api.json
-  python -mramyam.gen_client -y data/stripe-api.raml >stripe_api.py
-  python -mramyam.gen_server -y data/stripe-api.raml >stripe_svr.py
+  python -mramyam.gs -y stripe-api.raml -intermediate >gen/languages/js/stripe-api.json
+  python -mramyam.gs -y stripe-api.raml >stripe_api.py
+  #python -mramyam.gen_server -y data/stripe-api.raml >stripe_svr.py
 
   echo 'Trying python (twitter-rest-api.raml)...'
-  python -mramyam.gen_client -y data/twitter-rest-api.raml -intermediate >twitter_rest_api.json
-  python -mramyam.gen_client -y data/twitter-rest-api.raml >twitter_rest_api.py
-  python -mramyam.gen_server -y data/twitter-rest-api.raml >twitter_rest_svr.py
+  python -mramyam.gs -y twitter-rest-api.raml -intermediate >gen/languages/js/twitter-rest.json
+  python -mramyam.gs -y twitter-rest-api.raml >twitter_rest_api.py
+  #python -mramyam.gen_server -y data/twitter-rest-api.raml >twitter_rest_svr.py
 
   echo 'Trying python (twitter-api.raml)...'
-  python -mramyam.gen_client -y data/twitter.raml -intermediate >twitter_api.json
-  python -mramyam.gen_client -y data/twitter.raml >twitter_api.py
-  python -mramyam.gen_server -y data/twitter.raml >twitter_svr.py
+  python -mramyam.gs -y twitter.raml -intermediate >gen/languages/js/twitter.json
+  python -mramyam.gs -y twitter.raml >twitter_api.py
+  #python -mramyam.gen_server -y data/twitter.raml >twitter_svr.py
 
   echo Complete.
 fi
